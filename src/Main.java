7e371512b7728dc2c052c9e6be0f50ddb981bdfa
Prajwal-Somashekar-Gowda <78929626+Prajwal-Somashekar-Gowda@users.noreply.github.com>
2021-03-04 15:21:04 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args){
+                    //0   1   2   3   4
+        char[] arr ={'a','b','c','d','e'};
+
+        for(int i = 0;i<5;i++){
+            System.out.println("i: "+ i);
+            System.out.println(arr[i]);
+        }
+        //foreach loop
+        for(char ch: arr){
+            System.out.println(ch);
+        }
+
+        int charint = 0;
+
+        while ( (char)charint != 'g'){
+            System.out.println("int: "+ charint);
+            charint++;
+        }
+        System.out.println("char: "+ (char)charint);
+
+
+        //do-while loop
+        int index = 0;
+
+        do{
+            System.out.println(index++);
+        }while (index < 0);
+        System.out.println("index: " + index);
+
+    }
+}
